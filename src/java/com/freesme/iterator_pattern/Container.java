364c2f1365de399edb61273f0796616131b1137f
lm <freesme@163.com>
2019-11-27 10:21:32 +0800

    迭代器模式

---
+++ src/java/com/freesme/iterator_pattern/Container.java
@@ -0,0 +1,5 @@
+package com.freesme.iterator_pattern;
+
+public interface Container {
+    Iterator getIterator();
+}
---
+++ src/java/com/freesme/iterator_pattern/Domain.java
@@ -0,0 +1,12 @@
+package com.freesme.iterator_pattern;
+
+public class Domain {
+    public static void main(String[] args) {
+        NameRepository namesRepository = new NameRepository();
+
+        for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+            String name = (String)iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
